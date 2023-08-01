586b1d97baeb9da0b37593da6e74e916e4c7dd8b
Chris R <chcrowe@gmail.com>
2023-08-01 02:43:13 -0400

    Added buy tokens

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,8 +1,32 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "./Token.sol";
 
 contract Crowdsale {
     string public name = "Crowdsale";
+    Token public token;
+    uint256 public price;
+    uint256 public tokensSold;
+    uint256 public maxTokens;
+
+    event Buy(uint256 amount, address buyer);
+    // event Finalize(uint256 tokensSold, uint256 ethRaised);
+
+
+    constructor(Token _token, uint256 _price, uint256 _maxTokens) {
+        token = _token;
+        price = _price;
+        maxTokens = _maxTokens;
+    }
+
+    function buyTokens(uint256 _amount) public payable {
+        require(msg.value == (_amount / 1000000000000000000) * price);
+        require(token.balanceOf(address(this)) >= _amount);
+        require(token.transfer(msg.sender, _amount));
+
+        tokensSold += _amount;
+
+        emit Buy(_amount, msg.sender);
+    }
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -5,12 +5,32 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+const ether = tokens;
+
 describe('Crowdsale', () => {
-    let crowdsale
+    let crowdsale, token
+    let accounts, deployer, user1
 
     beforeEach(async () => {
+        // Load contracts
         const Crowdsale = await ethers.getContractFactory('Crowdsale')
-        crowdsale = await Crowdsale.deploy()
+        const Token = await ethers.getContractFactory('Token')
+
+        // Deploy token
+        token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
+        // Configure accounts
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+        user1 = accounts[1]
+
+        // deploy crowdsale
+        crowdsale = await Crowdsale.deploy(token.address, ether(1), '1000000')
+
+        // send tokens to crowdsale
+        let transaction = await token.connect(deployer).transfer(crowdsale.address, tokens(1000000))
+        await transaction.wait()
+
     })
 
     describe('Deployment', () => {
@@ -19,5 +39,56 @@ describe('Crowdsale', () => {
             expect(await crowdsale.name()).to.equal("Crowdsale")
         })
 
+        it('has correct token address', async () => {
+            expect(await crowdsale.token()).to.equal(token.address)
+        })
+
+        it('sends tokens to crowdsale contract', async () => {
+            expect(await token.balanceOf(crowdsale.address)).to.equal(tokens(1000000))
+        })
     })
+
+    // resume video at 20:34
+
+    describe('Buying tokens', () => {
+        let amount = tokens(10)
+        let transaction, result
+
+        describe('Success', () => {
+
+            beforeEach(async () => {
+                transaction = await crowdsale.connect(user1).buyTokens(amount, { value: ether(10) })
+                result = await transaction.wait()
+            })
+
+            it('transfers tokens', async () => {
+                expect(await token.balanceOf(crowdsale.address)).to.equal(tokens(999990))
+                expect(await token.balanceOf(user1.address)).to.equal(amount)
+            })
+
+            it('updates contract ether balance', async () => {
+                expect(await ethers.provider.getBalance(crowdsale.address)).to.equal(amount)
+            })
+
+            it('updates tokensSold', async () => {
+                expect(await crowdsale.tokensSold()).to.equal(amount)
+            })
+
+            it('emits a buy event', async () => {
+                // --> https://hardhat.org/hardhat-chai-matchers/docs/reference#.emit
+                await expect(transaction).to.emit(crowdsale, "Buy")
+                    .withArgs(amount, user1.address)
+            })
+
+        })
+
+        describe('Failure', () => {
+            it('rejects insufficent ETH', async () => {
+                await expect(crowdsale.connect(user1).buyTokens(tokens(10), { value: 0 })).to.be.reverted
+            })
+
+        })
+
+    })
+
 })
